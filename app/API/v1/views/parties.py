b34333c74e3ed12ab1bb61fa1ea3a304444ece9d
muirurikin <muirurikin@gmail.com>
2019-02-06 20:12:25 +0300

    [Delivers #163712112] Add create party endpoint

--- app/API/v1/views/parties.py
+++ app/API/v1/views/parties.py
@@ -6,3 +6,22 @@ from app.API.v1.models.parties import Party
 def get_parties():
   return Party.get_parties()
 
+@v1.route('/newParty', methods=['POST'])
+def new_party():
+  party_data = request.get_json()
+  party_name = party_data['name']
+  party_address = party_data['address']
+  party_image = party_data['logo']
+
+  new_party_info = {
+    "name": party_name,
+    "address": party_address,
+    "logo": party_image
+  }
+
+  Party.create_party(new_party_info)
+  
+  return make_response(jsonify({
+    "Message": "Party Info Added",
+    "Status": "Ok"
+  }), 201)
