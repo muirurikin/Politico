9b4665978cd3484d6ea9f55bffc86e8c2e085a7a
muirurikin <muirurikin@gmail.com>
2019-02-06 19:53:30 +0300

    [Finishes #163712112] Add create party endpoint

--- app/API/v1/views/parties.py
+++ app/API/v1/views/parties.py
@@ -5,3 +5,4 @@ from app.API.v1.models.parties import Party
 @v1.route('/parties', methods=['GET'])
 def get_parties():
   return Party.get_parties()
+
--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,10 @@ from flask import Flask
 from app.API.v1 import v1 as v1_blueprint
 from config import app_config
 
-def create_app(config_name):
+def create_app():
   app = Flask(__name__)
 
-  app.config.from_object(app_config[config_name])
+  app.config.from_object(app_config['development'])
   app.register_blueprint(v1_blueprint)
 
   return app
