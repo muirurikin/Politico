82732daedcd4fb60e220f51b0031e38312ca73dd
muirurikin <muirurikin@gmail.com>
2019-02-06 12:30:52 +0300

    [Finishes #163712634] Add create office endpoint

--- app/API/v1/views/offices.py
+++ app/API/v1/views/offices.py
@@ -1,7 +1,25 @@
 from flask import jsonify, make_response, request
 from app.API.v1 import v1
-from app.API.v1.models.offices import Office, offices
+from app.API.v1.models.offices import Office
 
 @v1.route('/offices', methods=['GET'])
 def get_offices():
-  return make_response(jsonify(offices))
+  return Office.get_offices()
+
+@v1.route('/new', methods=['POST'])
+def new_office():
+  office_data = request.get_json()
+  office_name = office_data['name']
+  office_type = office_data['type']
+
+  new_office_info = {
+    "name": office_name,
+    "type": office_type
+  }
+
+  Office.create_office(new_office_info)
+  
+  return make_response(jsonify({
+    "Message": "Office Info Added",
+    "Status": "Ok"
+  }), 201)
