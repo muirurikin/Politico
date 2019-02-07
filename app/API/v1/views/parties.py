9d6c062585ba65384f6dbf6724a13d2c8c36e3a8
muirurikin <muirurikin@gmail.com>
2019-02-07 23:11:44 +0300

    modified GET POST party endpoints

--- app/API/v1/views/parties.py
+++ app/API/v1/views/parties.py
@@ -2,39 +2,35 @@ from flask import jsonify, make_response, request
 from app.API.v1 import v1
 from app.API.v1.models.parties import Party, parties
 
-@v1.route('/parties', methods=['GET'])
-def get_parties():
-  return Party.get_parties()
+@v1.route('/parties', methods=['GET', 'POST'])
+def party_func():
+  if request.method == 'POST':
+    party_data = request.get_json()
+    party_id = party_data['id']
+    party_name = party_data['name']
+    party_address = party_data['address']
+    party_image = party_data['logo']
 
-@v1.route('/newParty', methods=['POST'])
-def new_party():
-  party_data = request.get_json()
-  party_id = party_data['id']
-  party_name = party_data['name']
-  party_address = party_data['address']
-  party_image = party_data['logo']
+    new_party_info = {
+      "id": party_id,
+      "name": party_name,
+      "address": party_address,
+      "logo": party_image
+    }
 
-  new_party_info = {
-    "id": party_id,
-    "name": party_name,
-    "address": party_address,
-    "logo": party_image
-  }
+    Party.create_party(new_party_info)
+    
+    return make_response(jsonify({
+      "Message": "Party Info Added",
+      "Status": "Ok"
+    }), 201)
+  else:
+    return Party.get_parties()
 
-  Party.create_party(new_party_info)
-  
-  return make_response(jsonify({
-    "Message": "Party Info Added",
-    "Status": "Ok"
-  }), 201)
-
-@v1.route('/party/<int:party_id>', methods=['GET'])
-def get_party(party_id):
-  for party in parties:
-    if party['id'] == party_id:
-      return make_response(jsonify(party))
-    else:
-      return None
+@v1.route('/party/<int:party_id>', methods=['GET', 'PUT', 'DELETE'])
+def party_func_id(party_id):
+  party1 = [party for party in parties if party['id'] == party_id]
+  return make_response(jsonify(party1[0]))
 
 @v1.route('/party/<int:party_id>', methods=['PUT'])
 def edit_party(party_id):
